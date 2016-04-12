f5f5db2a857b0d4d6ac1bce53240b37e267d8a66
liuze <aqliuze@163.com>
2016-04-12 10:46:09 +0800

    11111

---
+++ demo1/demo1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demo1
+//
+//  Created by 刘泽 on 16/4/12.
+//  Copyright © 2016年 刘泽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo1/demo1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demo1
+//
+//  Created by 刘泽 on 16/4/12.
+//  Copyright © 2016年 刘泽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
